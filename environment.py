7fb917f540f2ebf5e33ac2158a49df1e3a2d6b90
farismismar <faris_mismar@hotmail.com>
2019-04-09 16:55:53 -0500

    Add files via upload

--- environment.py
+++ environment.py
@@ -9,6 +9,7 @@ import math
 from gym import spaces, logger
 from gym.utils import seeding
 import numpy as np
+from numpy import linalg as LA
 
 # An attempt to follow
 # https://github.com/openai/gym/blob/master/gym/envs/classic_control/cartpole.py
@@ -60,16 +61,25 @@ class radio_environment:
         self.sinr_target = 2 # in dB
         self.min_sinr = -4 # in dB
         self.max_tx_power = 40 # in Watts
-        self.max_tx_interference = 40 # in Watts
+        self.max_tx_ue_2erference = 40 # in Watts
         self.f_c = 2100e6 # Hz
         self.prob_LOS = 0.3 # Probability of LOS transmission
+        self.G_ant = 17 # dBi mmWave antenna gain
         
+        # Where are the base stations?
+        self.x_bs_1, self.y_bs_1 = 0, 0
+        self.x_bs_2, self.y_bs_2 = self.inter_site_distance, 0
+
         self.num_actions = 8
         
         # for Beamforming
-        self.M_ULA = 64
-        self.k_oversampling = 2
-        
+        self.M_ULA = 4 # 64
+        self.k_oversample = 2  # oversampling factor
+        self.Np = 4 # from 3 to 5...
+        self.F = np.zeros([self.M_ULA, self.M_ULA], dtype=complex)        
+        self.theta_n = math.pi * np.arange(start=0., stop=1., step=1./(self.k_oversample*self.M_ULA))
+
+        # for Reinforcement Learning
         self.reward_min = -2
         self.reward_max = 100
         
@@ -87,7 +97,7 @@ class radio_environment:
             self.inter_site_distance+self.cell_radius,
             self.cell_radius,
             self.max_tx_power,
-            self.max_tx_interference])
+            self.max_tx_ue_2erference])
 
         self.action_space = spaces.Discrete(self.num_actions) # action size is here
         self.observation_space = spaces.Box(bounds_lower, bounds_upper, dtype=np.float32) # spaces.Discrete(2) # state size is here 
@@ -110,17 +120,23 @@ class radio_environment:
                       self.np_random.uniform(low=self.inter_site_distance-self.cell_radius, high=self.inter_site_distance+self.cell_radius),
                       self.np_random.uniform(low=-self.cell_radius, high=self.cell_radius),
                       self.np_random.uniform(low=1, high=self.max_tx_power),
-                      self.np_random.uniform(low=1, high=self.max_tx_interference)
+                      self.np_random.uniform(low=1, high=self.max_tx_ue_2erference)
                       ]
         
 #        self.steps_beyond_done = None
+        
+        # Beamforming matrix F
+        for n in np.arange(self.M_ULA):
+            f_n = self._compute_bf_vector(self.theta_n[n])
+            self.F[n,:] = f_n
+
         return np.array(self.state)
     
     def step(self, action):
         assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
         state = self.state
         reward = 0
-        x, y, x_int, y_int, pt_serving, pt_interferer = state
+        x, y, x_ue_2, y_ue_2, pt_serving, pt_interferer = state
 
         # based on the action make your call
         if (action == 0):
@@ -165,14 +181,17 @@ class radio_environment:
         dx_2 = v * math.cos(theta_2)
         dy_2 = v * math.sin(theta_2)
         
+        
+        # Move UE 1
         x += dx_1
         y += dy_1
         
-        x_int += dx_2
-        y_int += dy_2
+        # Move UE 2
+        x_ue_2 += dx_2
+        y_ue_2 += dy_2
         
-        received_power, interference_power, received_sinr = self._compute_rf(x, y, pt_serving, pt_interferer)
-        received_power_ue2, interference_power_ue2, received_ue2_sinr = self._compute_rf_user_2(x_int, y_int, pt_interferer, pt_serving) # the serving cell became interf. so did the PL
+        received_power, interference_power, received_sinr = self._compute_rf(x, y, pt_serving, pt_interferer, is_ue_2=False)
+        received_power_ue2, interference_power_ue2, received_ue2_sinr = self._compute_rf(x_ue_2, y_ue_2, pt_serving, pt_interferer, is_ue_2=True)
         
         # keep track of quantities...
         self.received_sinr_dB = received_sinr 
@@ -182,9 +201,9 @@ class radio_environment:
 
         # Did we find a FEASIBLE NON-DEGENERATE solution?
         done = (received_sinr >= self.sinr_target) and (pt_serving <= self.max_tx_power) and (pt_serving >= 0) and \
-                (pt_interferer <= self.max_tx_interference) and (pt_interferer >= 0) and (received_ue2_sinr >= self.sinr_target)
+                (pt_interferer <= self.max_tx_ue_2erference) and (pt_interferer >= 0) and (received_ue2_sinr >= self.sinr_target)
                 
-        abort = (pt_serving > self.max_tx_power) or (pt_interferer > self.max_tx_interference) or \
+        abort = (pt_serving > self.max_tx_power) or (pt_interferer > self.max_tx_ue_2erference) or \
                         (received_sinr <= self.min_sinr) or (received_ue2_sinr <= self.min_sinr) or (received_sinr > 30) or (received_ue2_sinr > 30) # SINR > 30 dB is unrealistic.
                 
         print('{:.2f} dB | {:.2f} dB | {:.2f} W | {:.2f} W '.format(received_sinr, received_ue2_sinr, pt_serving, pt_interferer), end='')
@@ -192,7 +211,7 @@ class radio_environment:
 #        print('UE moved to ({0:0.3f},{1:0.3f}) and their received SINR became {2:0.3f} dB.'.format(x,y,received_sinr))
         
         # Update the state.
-        self.state = (x,y, x_int, y_int, pt_serving, pt_interferer)
+        self.state = (x,y, x_ue_2, y_ue_2, pt_serving, pt_interferer)
      
         if done:
             reward += self.reward_max
@@ -201,130 +220,120 @@ class radio_environment:
             
         return np.array(self.state), reward, done, abort
 
-    def _compute_bf_vector(self, theta_n):
+    def _compute_bf_vector(self, theta):
         c = 3e8 # speed of light
         wavelength = c / self.f_c
         
         d = wavelength / 2. # antenna spacing 
         k = 2. * math.pi / wavelength
+    
+        exponent = 1j * k * d * math.cos(theta) * np.arange(self.M_ULA)
         
-        exponent = 1j * k * d * math.cos(theta_n)
+        f = 1. / math.sqrt(self.M_ULA) * np.exp(exponent)
         
-        f = 1. / math.sqrt(self.M_ULA) * np.exp(exponent * np.arange(self.M_ULA, dtype=complex))
-        
+        # Test the norm square... is it equal to unity? YES.
+    #    norm_f_sq = LA.norm(f, ord=2) ** 2
+     #   print(norm_f_sq)
+    
         return f
 
-    # TODO: build the same function for the second user
-    def _compute_channel(self, Np, theta, x_ue, y_ue, x_bs, y_bs):
+    def _compute_channel(self, x_ue, y_ue, x_bs, y_bs):
         # Np is the number of paths p
-        # theta is the steering angle
-
-        path_loss = self._path_loss(x_ue, y_ue, x_bs, y_bs)
-
+        PLE_L = 2
+        PLE_N = 4
+        G_ant = 17 # dBi
+        
+        # theta is the steering angle.  Sampled iid from unif(0,2pi).
+        theta = np.random.uniform(low=0, high=2*math.pi, size=self.Np)
+    
+        path_loss_LOS = 10 ** (self._path_loss_mmWave(x_ue, y_ue, PLE_L, x_bs, y_bs) / 10.)
+        path_loss_NLOS = 10 ** (self._path_loss_mmWave(x_ue, y_ue, PLE_N, x_bs, y_bs) / 10.)
+                            
+        ## just changed alpha to be complex in the case of NLOS
+        alpha = np.ones(self.Np) * (self.prob_LOS / path_loss_LOS  + (1 - self.prob_LOS) * (np.complex(np.random.normal(),np.random.normal())) / path_loss_NLOS)
+    
+        # TODO: Ahmad needs to comment: Does this make sense?
+        rho = 1 / path_loss_LOS * 10 ** (G_ant / 10.)
+        
         # initialize the channel as a complex variable.
         h = np.zeros(self.M_ULA, dtype=complex)
-        alpha = np.ones(Np) * self.prob_LOS * 1 + (1 - self.prob_LOS) * np.random.normal()
-        a_theta = self._compute_bf_vector(theta)
-        for p in np.arange(Np):
-            h += alpha[p] * a_theta.T
         
-        h *= math.sqrt(self.M_ULA) / path_loss
+        for p in np.arange(self.Np):
+            a_theta = self._compute_bf_vector(theta[p])
+            h += alpha[p] / rho * a_theta.T # scalar multiplication into a vector
+        
+        h *= math.sqrt(self.M_ULA)
         
         return h
-        
-    def _compute_rf(self, x, y, pt_serving, pt_interferer):
-        # Without loss of generality, the base station is at the origin
-        # The interfering base station is x = cell_radius, y = 0
-        x_bs_int = self.inter_site_distance
-        y_bs_int = 0
-        
-        ue_noise_figure = 7 # dB
+
+    # TODO: build the same function for the second user
+    def _compute_rf(self, x_ue, y_ue, pt_bs1, pt_bs2, is_ue_2=False):
         T = 290 # Kelvins
-        B = 15000 # Hz
-        k = 1.38e-23
-        g_ant = 17 # dBi
-        NRB = 25 # 5 MHz channel
+        B = 50e6 # Hz
+        k_Boltzmann = 1.38e-23
+        
+        noise = np.random.normal(0, k_Boltzmann*T*B) # this is in Watts
+        
+        # TODO: Ahmad needs to comment: what is the proper n?
+        n = 1
+        
+        # Lookup the beamforming vector f_n
+        f_n = self.F[:,n]
+        
+        if is_ue_2 == False:
+            # Without loss of generality, the base station is at the origin
+            # The interfering base station is x = cell_radius, y = 0
+            x_bs_1, y_bs_1 = self.x_bs_1, self.y_bs_1
+            x_bs_2, y_bs_2 = self.x_bs_2, self.y_bs_2
 
-        # Find the received powers
-        path_loss = self._path_loss(x,y)
-        int_path_loss = self._path_loss(x,y,x_bs_int,y_bs_int)
-        
-        noise_power = k*T*B*10**(ue_noise_figure/10.) # Watts
-        interference_power = pt_interferer / NRB * 10**(-int_path_loss/10.) * 10**(g_ant/10.) # Watts
-        
-        received_power = 10*np.log10(pt_serving*1e3) -10*np.log(NRB) + g_ant - path_loss
-        interference_plus_noise_power = 10*np.log10((noise_power + interference_power)*1e3)
+            # Now the channel h, which is a vector in beamforming.
+            # This computes the channel for user in serving BS from the serving BS.
+            h_1 = self._compute_channel(x_ue, y_ue, x_bs=x_bs_1, y_bs=y_bs_1) 
+            # This computes the channel for user in serving BS from the interfering BS.
+            h_2 = self._compute_channel(x_ue, y_ue, x_bs=x_bs_2, y_bs=y_bs_2) 
+            received_power = pt_bs1 * abs(np.dot(h_1.conj(), f_n)) ** 2
+            interference_power = pt_bs2 * abs(np.dot(h_2.conj(), f_n)) ** 2
 
-#        print(received_power, interference_plus_noise_power) # before fading, ok
+        else:
+            x_bs_1, y_bs_1 = self.x_bs_1, self.y_bs_1
+            x_bs_2, y_bs_2 = self.x_bs_2, self.y_bs_2            
+            
+            # Now the channel h, which is a vector in beamforming.
+            # This computes the channel for user in serving BS from the serving BS.
+            h_1 = self._compute_channel(x_ue, y_ue, x_bs=x_bs_2, y_bs=y_bs_2) 
+            # This computes the channel for user in serving BS from the interfering BS.
+            h_2 = self._compute_channel(x_ue, y_ue, x_bs=x_bs_1, y_bs=y_bs_1) 
+            received_power = pt_bs2 * abs(np.dot(h_1.conj(), f_n)) ** 2
+            interference_power = pt_bs1 * abs(np.dot(h_2.conj(), f_n)) ** 2
+            
+        interference_plus_noise_power = interference_power + noise
         
-        # TODO: Find the Rician coefficient formula and add it for the LOS component.
-        
-        # Add Rayleigh fading and log-normal fading here.
-        h = np.random.normal(size=2) / math.sqrt(2)
-        rayleigh_coeff = (1 - self.prob_LOS) * 10*np.log10(np.linalg.norm(h) ** 2) + self.prob_LOS * 0.
-        sf_margin = 2 # dB
-        log_normal_fading = np.random.normal(0, sf_margin)
-        received_power -= rayleigh_coeff + log_normal_fading
-        
-        h = np.random.normal(size=2) / math.sqrt(2)
-        rayleigh_coeff = (1 - self.prob_LOS) * 10*np.log10(np.linalg.norm(h) ** 2) + self.prob_LOS * 0.
-        sf_margin = 2 # dB
-        log_normal_fading = np.random.normal(0, sf_margin)
-        interference_plus_noise_power -= rayleigh_coeff + log_normal_fading
-        
-        received_sinr = received_power - interference_plus_noise_power
-        
-#        print(pt_serving)
-
-        
-#        print(pt_serving, received_sinr) # after fading, ?
-                
-       # print(10*np.log10(1e3*pt_serving), path_loss, received_power)  # this all ok.
-
-
+        received_sinr = 10*np.log10(received_power / interference_plus_noise_power)
+               
         return [received_power, interference_power, received_sinr]
     
-    def _compute_rf_user_2(self, x_int, y_int, pt_serving, pt_interferer):
-        # Without loss of generality, the base station is at the origin
-        # The interfering base station is x = cell_radius, y = 0
-        x_bs_int = self.inter_site_distance
-        y_bs_int = 0
-        
-        ue_noise_figure = 7 # dB
-        T = 290 # Kelvins
-        B = 15000 # Hz
-        k = 1.38e-23
-        g_ant = 17 # dBi
-        NRB = 25 # 5 MHz channel
-        
-         # Find the received powers
-        path_loss = self._path_loss(x_int, y_int, x_bs_int, y_bs_int) # this is the pathloss from bs_int to ue_2
-        int_path_loss = self._path_loss(x_int, y_int) # pathloss from bs to ue_2
-        
-        noise_power = k*T*B*10**(ue_noise_figure/10.) # Watts
-        interference_power = pt_interferer / NRB * 10**(-int_path_loss/10.) * 10**(g_ant/10.) # Watts
-        
-        received_power = 10*np.log10(pt_serving*1e3) -10*np.log(NRB) + g_ant - path_loss
-        interference_plus_noise_power = 10*np.log10((noise_power + interference_power)*1e3)
-
-        # Add Rayleigh fading and log-normal fading here.
-        h = np.random.normal(size=2) / math.sqrt(2)
-        rayleigh_coeff = 10*np.log10(np.linalg.norm(h)) ** 2
-        sf_margin = 2 # dB
-        log_normal_fading = np.random.normal(0, sf_margin)        
-        received_power -= rayleigh_coeff + log_normal_fading
-        
-        h = np.random.normal(size=2) / math.sqrt(2)
-        rayleigh_coeff = 10*np.log10(np.linalg.norm(h)) ** 2
-        sf_margin = 2 # dB
-        log_normal_fading = np.random.normal(0, sf_margin)
-        interference_plus_noise_power -= rayleigh_coeff + log_normal_fading
-        
-        received_sinr = received_power - interference_plus_noise_power
-
-        return [received_power, interference_power, received_sinr]
     
-    def _path_loss(self, x, y, x_bs=0, y_bs=0):
+    # https://ieeexplore-ieee-org.ezproxy.lib.utexas.edu/stamp/stamp.jsp?tp=&arnumber=7522613
+    def _path_loss_mmWave(self, x, y, PLE, x_bs=0, y_bs=0):
+        # These are the parameters for f = 28000 MHz.
+        c = 3e8 # speed of light
+        wavelength = c / self.f_c
+        A = 0.0671
+        Nr = 1
+        sigma_sf = 9.1
+        #PLE = 3.812
+        
+        d = math.sqrt((x - x_bs)**2 + (y - y_bs)**2) # in meters
+        
+        fspl = 10 * np.log10(((4*math.pi*d) / wavelength) ** 2)
+        pl = fspl + 10 * np.log10(d ** PLE) * (1 - A*np.log2(Nr))
+    
+        chi_sigma = np.random.normal(0, sigma_sf) # log-normal shadowing 
+        L = pl + chi_sigma
+    
+        return L # in dB    
+        
+    def _path_loss_sub6(self, x, y, x_bs=0, y_bs=0):
         f_c = self.f_c
         c = 3e8 # speed of light
         d = math.sqrt((x - x_bs)**2 + (y - y_bs)**2)
