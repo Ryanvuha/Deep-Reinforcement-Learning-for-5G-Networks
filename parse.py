63a3fdfbd953dc21fe60fbbcfd673f5ddb39fc9a
farismismar <faris_mismar@hotmail.com>
2019-08-24 17:02:23 -0500

    Add files via upload

--- parse.py
+++ parse.py
@@ -3,14 +3,10 @@
 
 import glob
 import re
-import numpy as np
 
 files = glob.glob('measurements*.txt')
-f1 = open('ue_1_sinr.txt', 'a')
-f2 = open('ue_2_sinr.txt', 'a')
-f3 = open('ue_1_tx_power.txt', 'a')
-f4 = open('ue_2_tx_power.txt', 'a')
-
+f1 = open('ue_1.txt', 'a')
+f2 = open('ue_2.txt', 'a')
 
 pattern = re.compile('[\[\]_ \':a-z]+') # get rid of [], colons, and words.
 
@@ -19,8 +15,6 @@ for file in files:
     lines = f.read()
     sinr1 = lines.split(':')[1]
     sinr2 = lines.split(':')[2]
-    txpower1 = lines.split(':')[3]
-    txpower2 = lines.split(':')[4]
     
     if ('nan' in sinr1) or ('nan' in sinr2):
         continue
@@ -29,15 +23,7 @@ for file in files:
     f1.write('{},'.format(re.sub(pattern, '', sinr1)))
     f2.write('{},'.format(re.sub(pattern, '', sinr2)))
 
-    # Clean up tx1, 2 by replacing pattern with ''
-    f3.write('{},'.format(re.sub(pattern, '', txpower1)))
-    f4.write('{},'.format(re.sub(pattern, '', txpower2)))
-
-f4.close()
-f3.close()
-f2.close()
 f1.close()
+f2.close()
 f.close()
 
-
-	
