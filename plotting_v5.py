ea9383c9a1f416d942b687766880afe1b46a015a
farismismar <faris_mismar@hotmail.com>
2019-09-08 14:06:38 -0500

    Add files via upload

--- plotting_v5.py
+++ plotting_v5.py
@@ -19,7 +19,7 @@ import matplotlib2tikz
 
 os.chdir('/Users/farismismar/Desktop/deep')
 
-MAX_EPISODES_DEEP = 1050
+MAX_EPISODES_DEEP = 1000
 MIN_EPISODES_DEEP = 500
 
 def sum_rate(sinr):
@@ -41,7 +41,7 @@ def plot_ccdf(T, labels, filename='ccdf'):
         r'\usepackage{amsmath}',
         r'\usepackage{amssymb}']   
     
-    num_bins =  20
+    num_bins = 10
     i = 0
     for data in T:
         data_ = T[data].dropna()
@@ -61,7 +61,7 @@ def plot_ccdf(T, labels, filename='ccdf'):
     ax.set_ylabel('$1 - F_\Gamma(\gamma)$')        
     ax.legend(labels, loc="lower left")
     plt.savefig('{}.pdf'.format(filename), format='pdf')
-    matplotlib2tikz.save('figures/{}.tikz'.format(filename))
+    matplotlib2tikz.save('{}.tikz'.format(filename))
     plt.close(fig)    
     
 def plot_primary(X,Y, xlabel, ylabel, ymin=0, ymax=MAX_EPISODES_DEEP, filename='plot.pdf'):
@@ -76,13 +76,13 @@ def plot_primary(X,Y, xlabel, ylabel, ymin=0, ymax=MAX_EPISODES_DEEP, filename='
     
     plt.xlabel(xlabel)
     
-    X = np.log2(np.array(X))
+    #X = np.log2(np.array(X))
     
     ax = fig.gca()
     
-    ax.xaxis.set_major_locator(MultipleLocator(1))
+    #ax.xaxis.set_major_locator(MultipleLocator(1))
     # Format the ticklabel to be 2 raised to the power of `x`
-    ax.xaxis.set_major_formatter(FuncFormatter(lambda x, pos: int(2**x)))
+    #ax.xaxis.set_major_formatter(FuncFormatter(lambda x, pos: int(2**x)))
     
     ax.set_autoscaley_on(False)
     
@@ -94,10 +94,11 @@ def plot_primary(X,Y, xlabel, ylabel, ymin=0, ymax=MAX_EPISODES_DEEP, filename='
 
     fig.tight_layout()
     plt.savefig('{}'.format(filename), format='pdf')
-    matplotlib2tikz.save('figures/{}.tikz'.format(filename))
+    matplotlib2tikz.save('{}.tikz'.format(filename))
 
 ##############################
-def compute_distributions(optimal=False, cutoff=0):
+
+def compute_distributions(optimal=False):
     df_final = pd.DataFrame()
     
     for M in [4, 8, 16, 32, 64]:
@@ -108,11 +109,12 @@ def compute_distributions(optimal=False, cutoff=0):
             df_1 = pd.read_csv('figures_M={}/ue_1_sinr.txt'.format(M), sep=',', header=None, index_col=False).transpose().dropna()
             df_2 = pd.read_csv('figures_M={}/ue_2_sinr.txt'.format(M), sep=',', header=None, index_col=False).transpose().dropna()
         
+        cutoff=5 + 10*np.log2(M)
         df = pd.concat([df_1, df_2], axis=0, ignore_index=True)
         df = df.astype(float)
         sinr = np.array(df)
-#        indexes = (sinr >= cutoff)
-        df_sinr = pd.DataFrame(sinr)
+        indexes = (sinr <= cutoff)
+        df_sinr = pd.DataFrame(sinr[indexes])
         df_sinr.columns = ['sinr_{}'.format(M)]
         
         if optimal:
@@ -126,7 +128,7 @@ def compute_distributions(optimal=False, cutoff=0):
         df = df.astype(float)
         
         tx_power = np.array(df)
-        df_power = pd.DataFrame(tx_power)
+        df_power = pd.DataFrame(tx_power[indexes])
         df_power.columns = ['tx_power_{}'.format(M)]
     
         df_final = pd.concat([df_final, df_sinr, df_power], axis=1)
@@ -149,9 +151,9 @@ def plot_secondary(X,Y1, Y2, Y3, Y4, xlabel, y1label, y2label, y1max, y2max, fil
     plt.grid(True, axis='both', which='both')
         
     ax = fig.gca()
-    ax.xaxis.set_major_locator(MultipleLocator(1))
+#    ax.xaxis.set_major_locator(MultipleLocator(1))
     # Format the ticklabel to be 2 raised to the power of `x`
-    ax.xaxis.set_major_formatter(FuncFormatter(lambda x, pos: int(2**x)))
+ #   ax.xaxis.set_major_formatter(FuncFormatter(lambda x, pos: int(2**x)))
     
     ax.set_autoscaley_on(False)
     ax_sec = ax.twinx()
@@ -164,14 +166,14 @@ def plot_secondary(X,Y1, Y2, Y3, Y4, xlabel, y1label, y2label, y1max, y2max, fil
     ax.set_ylabel(y1label)
     ax_sec.set_ylabel(y2label)
     
-    ax.set_ylim(30, y1max)
-    ax_sec.set_ylim(20, y2max)
+    ax.set_ylim(40, y1max)
+    ax_sec.set_ylim(60, y2max)
     
     plt.grid(True)
-    plt.legend([plot1_, plot2_, plot3_, plot4_], ['TX Power JB-PCIC', 'TX Power Optimal', 'SINR JB-PCIC', 'SINR Optimal'], loc='lower left')
+    plt.legend([plot1_, plot2_, plot3_, plot4_], ['TX Power JB-PCIC', 'TX Power Optimal', 'SINR JB-PCIC', 'SINR Optimal'], loc='best')
     fig.tight_layout()
     plt.savefig('{}'.format(filename), format='pdf')
-    matplotlib2tikz.save('figures/{}.tikz'.format(filename))
+    matplotlib2tikz.save('{}.tikz'.format(filename))
     
 def plot_primary_two(X, Y1, Y2, xlabel, ylabel, filename='plot.pdf'):
     fig = plt.figure(figsize=(10.24,7.68))
@@ -186,9 +188,9 @@ def plot_primary_two(X, Y1, Y2, xlabel, ylabel, filename='plot.pdf'):
     plt.xlabel(xlabel)
     
     ax = fig.gca()
-    ax.xaxis.set_major_locator(MultipleLocator(1))
+#    ax.xaxis.set_major_locator(MultipleLocator(1))
     # Format the ticklabel to be 2 raised to the power of `x`
-    ax.xaxis.set_major_formatter(FuncFormatter(lambda x, pos: int(2**x)))
+ #   ax.xaxis.set_major_formatter(FuncFormatter(lambda x, pos: int(2**x)))
     
     ax.set_autoscaley_on(False)
     
@@ -196,13 +198,13 @@ def plot_primary_two(X, Y1, Y2, xlabel, ylabel, filename='plot.pdf'):
     plot2_, = ax.plot(X, Y2, 'ro--')
 
     ax.set_ylabel(ylabel)
-    ax.set_ylim(0, max(max(Y1), max(Y2))*1.02)
+    ax.set_ylim(min(min(Y1), min(Y2))*0.98, max(max(Y1), max(Y2))*1.02)
     
     plt.grid(True)
-    plt.legend([plot1_, plot2_], ['JB-PCIC', 'Optimal'], loc='lower left')
+    plt.legend([plot1_, plot2_], ['JB-PCIC', 'Optimal'], loc='best')
     fig.tight_layout()
     plt.savefig('{}'.format(filename), format='pdf')
-    matplotlib2tikz.save('figures/{}.tikz'.format(filename))
+    matplotlib2tikz.save('{}.tikz'.format(filename))
     
 ##############################
 df_final_ = compute_distributions()
@@ -226,18 +228,19 @@ sinr_64= sinr_64[~np.isnan(sinr_64)]
 tx_power_64= tx_power_64[~np.isnan(tx_power_64)]
 
 plot_ccdf(df_final_[['sinr_4', 'sinr_8', 'sinr_16', 'sinr_32', 'sinr_64']], [r'$M_\text{ULA} = 4$', r'$M_\text{ULA} = 8$', r'$M_\text{ULA} = 16$', r'$M_\text{ULA} = 32$', r'$M_\text{ULA} = 64$'])
+q = 100
 
-tx_power_4_agg = 10*np.log10(10 ** (np.max(tx_power_4)/10.))
-tx_power_8_agg = 10*np.log10(10 ** (np.max(tx_power_8)/10.))
-tx_power_16_agg = 10*np.log10(10 ** (np.max(tx_power_16)/10.))
-tx_power_32_agg = 10*np.log10(10 ** (np.max(tx_power_32)/10.))
-tx_power_64_agg = 10*np.log10(10 ** (np.max(tx_power_64)/10.))
+tx_power_4_agg = 10*np.log10(10 ** (np.nanpercentile(tx_power_4, q)/10.))
+tx_power_8_agg = 10*np.log10(10 ** (np.nanpercentile(tx_power_8, q)/10.))
+tx_power_16_agg = 10*np.log10(10 ** (np.nanpercentile(tx_power_16, q)/10.))
+tx_power_32_agg = 10*np.log10(10 ** (np.nanpercentile(tx_power_32, q)/10.))
+tx_power_64_agg = 10*np.log10(10 ** (np.nanpercentile(tx_power_64, q)/10.))
 
-sinr_avg_4 = 10*np.log10(10 ** (np.max(sinr_4)/10.))
-sinr_avg_8 = 10*np.log10(10 ** (np.max(sinr_8)/10.))
-sinr_avg_16 = 10*np.log10(10 ** (np.max(sinr_16)/10.))
-sinr_avg_32 = 10*np.log10(10 ** (np.max(sinr_32)/10.))
-sinr_avg_64 = 10*np.log10(10 ** (np.max(sinr_64)/10.))
+sinr_avg_4 = 10*np.log10(10 ** (np.nanpercentile(sinr_4, q)/10.))
+sinr_avg_8 = 10*np.log10(10 ** (np.nanpercentile(sinr_8, q)/10.))
+sinr_avg_16 = 10*np.log10(10 ** (np.nanpercentile(sinr_16, q)/10.))
+sinr_avg_32 = 10*np.log10(10 ** (np.nanpercentile(sinr_32, q)/10.))
+sinr_avg_64 = 10*np.log10(10 ** (np.nanpercentile(sinr_64, q)/10.))
 
 ##############################
 
@@ -262,10 +265,11 @@ for M in X:
 
 Y.to_csv('convergence_vs_ant.csv', index=False)
 df_convergence = Y.groupby('M').mean().reset_index()
+#df_convergence = Y.groupby('M').quantile(0.8).reset_index()
 df_convergence.columns = ['M', 'episode']
 plot_primary(df_convergence['M'], df_convergence['episode'], r'$M$', r'Convergence Episode ($\zeta$)', MIN_EPISODES_DEEP, MAX_EPISODES_DEEP, 'convergence.pdf')
 
-X=np.log2(np.array(X)).astype(int)
+#X=np.log2(np.array(X)).astype(int)
 tx_power_4_optimal_agg = 10*np.log10(10 ** (np.nanmax(tx_power_4_optimal) / 10.))
 tx_power_8_optimal_agg = 10*np.log10(10 ** (np.nanmax(tx_power_8_optimal) / 10.))
 tx_power_16_optimal_agg = 10*np.log10(10 ** (np.nanmax(tx_power_16_optimal) / 10.))
